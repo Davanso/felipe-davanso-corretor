e4064922b855937b0355889dba27c6899b04e575
Guilherme <gui.social.ti@gmail.com>
2024-08-30 14:05:23 -0300

    Add initial files and header design

---
+++ JS/menu-lateral.js
@@ -0,0 +1,7 @@
+document.getElementById("menu-icon").onclick = function() {
+    document.getElementById("side-menu").style.width = "250px";
+}
+
+document.querySelector(".close-btn").onclick = function() {
+    document.getElementById("side-menu").style.width = "0";
+}
