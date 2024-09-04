45f700238802ca5c6c0716a53baf6de6a621371a
Guilherme <gui.social.ti@gmail.com>
2024-09-04 16:07:34 -0300

    Add more content and script for auto-video

---
+++ JS/auto-video.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const videoGrid = document.querySelector('.video-grid');
+    const videos = Array.from(videoGrid.getElementsByClassName('video-container'));
+    let currentIndex = 0;
+    const carouselInterval = 3000;
+    const startDelay = 5000;
+
+    function showCurrentVideo() {
+        const videoWidth = videos[0].offsetWidth + 20; // Inclui o espaço entre os vídeos
+        const offset = -currentIndex * videoWidth; // Ajusta o deslocamento baseado na largura do vídeo
+        videoGrid.style.transform = `translateX(${offset}px)`;
+    }
+
+    function startCarousel() {
+        showCurrentVideo();
+        setInterval(() => {
+            currentIndex = (currentIndex + 1) % videos.length; // Move para o próximo vídeo e volta ao início
+            showCurrentVideo();
+        }, carouselInterval); // Troca de vídeo a cada x segundos determinado pelo carouselInterval
+    }
+
+    // Inicio pos atraso
+    setTimeout(startCarousel, startDelay);
+});
